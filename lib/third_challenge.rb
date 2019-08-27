10efc3ea57bb30e724b6888fda77e493c19e5dae
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 13:27:34 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,8 @@ def third_challenge
         },
       :hero => {
         name: "Romeo",
-        age: "54"
+        age: "15",
+        status: "alive"
         },
       :hero_friends => []
    }, 
