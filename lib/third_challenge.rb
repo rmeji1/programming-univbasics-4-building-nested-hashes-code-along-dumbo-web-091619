7c92dde0d1ab4e27a25cb3a190381a8e9153864f
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 13:28:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
         age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        
         },
       :heroine => {
         #your key/value pairs here
